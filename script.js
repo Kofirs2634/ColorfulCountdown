bb3c55597e40e6299be787c7ff4ef50b79544a6d
family <family@Air-Andrey.Dlink>
2021-09-09 16:27:35 +0300

    these changes too

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-const MAX_TIME = parseInt(prompt('Сколько времени считаем?', 60))
+;(function() {
+    const MAX_TIME = setTime()
 
-let time = MAX_TIME
-let timer = setInterval(() => {
-	time -= 1
-	updateRing()
-	updateLabel()
-	if (time <= 0) clearInterval(timer)
-	console.log(time)
-}, 1000)
+    let time = MAX_TIME
+    let ringProgr = MAX_TIME * 100
 
-function updateRing() {
-	let progr = time / MAX_TIME
-	document.querySelector('.ring').style.setProperty('--progress', progr + 'turn')
-}
-function updateLabel() {
-	let mins = Math.floor(time / 60).toString()
-	let secs = Math.round(time % 60).toString()
-	document.querySelector('.count').innerText = `${'00'.substr(mins.length) + mins}:${'00'.substr(secs.length) + secs}`
-}
+    updateLabel()
+
+    let timer = setInterval(() => {
+        time -= 1
+        updateLabel()
+        if (time == 0) {
+            alert('Отсчет закончился!')
+            clearInterval(timer)
+        }
+    }, 1000)
+
+    let ringTimer = setInterval(() => {
+        ringProgr -= 1
+        updateRing()
+        if (ringProgr == 0) clearInterval(ringTimer)
+    }, 10)
+
+    function setTime() {
+        let time = parseInt(prompt('Сколько времени считаем (в секундах)?', 60))
+        while (time <= 0 || time > 360000) {
+            time = parseInt(prompt('Нет, минимум можно поставить 1 секунду, а максимум - 100 часов. Так сколько считаем?', 60))
+        }
+        return time
+    }
+
+    function updateRing() {
+        let progr = ringProgr / (MAX_TIME * 100)
+        document.querySelector('.ring').style.setProperty('--progress', progr + 'turn')
+    }
+
+    function updateLabel() {
+        let hrs = Math.floor(time / 3600).toString()
+        let mins = Math.floor(time % 3600 / 60).toString()
+        let secs = Math.ceil(time % 60).toString()
+        document.querySelector('.count').innerText = `${hrs.padStart(2, '0')}:${mins.padStart(2, '0')}:${secs.padStart(2, '0')}`
+    }
+})();
